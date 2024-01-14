94434798ed83678814bce4d71673b2bca3de6389
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-01-14 11:22:27 +0000

    Add heroku to allowed hosts

--- babytimer/settings.py
+++ babytimer/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,6 +30,7 @@ DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    'baby-timer-15aeb8ae861d.herokuapp.com',
 ]
 
 
