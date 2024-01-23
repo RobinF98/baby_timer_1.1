30e922b919270755db2c0a46e85758e8d64fff55
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-01-23 13:22:09 +0000

    Add baby list view and urls

--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import baby
 # Create your views here.
+
+
+class BabyList(generic.ListView):
+    model = baby
+    template_name = 'logs/index.html'
+
+    def get_queryset(self):
+        return baby.objects.filter(user=self.request.user)
+
+# class BabyCreate(generic.edit.CreateView):
+#     model = baby
