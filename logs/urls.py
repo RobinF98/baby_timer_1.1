a6cea4823d011151eed05d5ece70a88fbf8b03ba
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-02-09 22:09:03 +0000

    Add BabyCreateView to views.py and wire up urls and template

--- logs/urls.py
+++ logs/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.BabyListView.as_view(), name="home"),
     path("baby/<int:pk>", views.BabyDetailView.as_view(), name="baby-detail"),
+    path("add_baby", views.BabyCreateView.as_view(), name="add_baby")
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,19 +1,54 @@
-from django.shortcuts import render, get_object_or_404
+from typing import Any
+from urllib import request
+from django import forms
+from django.forms.models import BaseModelForm
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views import generic, View
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.models import User
+from django.urls import reverse, reverse_lazy
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput, DatePickerInput
+from requests import options
 from .models import Baby
 # Create your views here.
 
 
-class BabyListView(generic.ListView):
+class BabyListView(LoginRequiredMixin, generic.ListView):
     model = Baby
     template_name = 'logs/index.html'
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
 
-# class BabyCreate(generic.edit.CreateView):
-#     model = baby
-
 
 class BabyDetailView(generic.detail.DetailView):
     model = Baby
+
+
+class BabyCreateView(generic.edit.CreateView):
+    # set user field to current user
+
+    model = Baby
+    fields = [
+        "baby_name",
+        "birthday",
+        "due_date",
+    ]
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields["birthday"].widget = DatePickerInput()
+        form.fields["due_date"].widget = DatePickerInput()
+        return form
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        baby = form.save()
+        return HttpResponseRedirect(reverse("home"))
+
+    # def get_success_url(self):
+    # #     return HttpResponseRedirect(reverse("home"))
+    # def post(self, request):
+    #     baby = BabyCreateView.save()
+    #     return HttpResponseRedirect(reverse("home"))
