1c0c1c0f2f33844ef19b6586245fffbb59e994f5
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-02-12 13:14:11 +0000

    Add update view url to urls.py

--- logs/urls.py
+++ logs/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.BabyListView.as_view(), name="home"),
     path("baby/<int:pk>", views.BabyDetailView.as_view(), name="baby-detail"),
-    path("add_baby", views.BabyCreateView.as_view(), name="add_baby")
+    path("add_baby", views.BabyCreateView.as_view(), name="add-baby"),
+    path("edit_baby/<pk>", views.BabyUpdateView.as_view(), name="edit-baby")
 ]
