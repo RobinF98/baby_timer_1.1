f88e244f66185401ce8f145377fbf047f48a1c75
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-02-26 23:08:28 +0000

    Edit time datetime fields defaults for logs models

--- logs/models.py
+++ logs/models.py
@@ -35,7 +35,7 @@ class Diaper(models.Model):
         "WD": "Wet and Dirty"
     }
     baby = models.ForeignKey(Baby, on_delete=models.CASCADE, blank=False,)
-    time = models.DateTimeField(default = datetime.now, blank=False)
+    time = models.DateTimeField(default=datetime.now, blank=False)
     type = models.CharField(
         choices=DIAPER_TYPE_CHOICES,
         default="WT",
@@ -46,7 +46,7 @@ class Diaper(models.Model):
 
 class Sleep(models.Model):
     baby = models.ForeignKey(Baby, on_delete=models.CASCADE, blank=False,)
-    start_time = models.DateTimeField("Sleep start time", auto_now_add=True, blank=False,)
+    start_time = models.DateTimeField("Sleep start time", default=datetime.now, blank=False,)
     end_time = models.DateTimeField("Sleep end time", blank=False,)
     notes = models.TextField(blank=True,)
 
