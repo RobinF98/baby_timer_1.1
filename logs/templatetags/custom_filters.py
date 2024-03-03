773730a8ddba804512eeabe2b53d8c7de3b6e672
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-03-03 10:49:28 +0000

    Add custom filters for logs template

--- logs/templatetags/custom_filters.py
+++ logs/templatetags/custom_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def to_class_name(value):
     return value.__class__.__name__
+
+@register.filter
+def to_edit_url(value):
+    return 'edit-'+str(value).lower()
