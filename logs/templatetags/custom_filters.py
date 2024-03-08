e5c7480681c5ca36bd247a14863895e30c6f4819
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-03-08 03:13:29 +0000

    Add filter to get create edit url string

--- logs/templatetags/custom_filters.py
+++ logs/templatetags/custom_filters.py
@@ -7,6 +7,12 @@ register = template.Library()
 def to_class_name(value):
     return value.__class__.__name__
 
+
 @register.filter
 def to_edit_url(value):
     return 'edit-'+str(value).lower()
+
+
+@register.filter
+def to_delete_url(value):
+    return 'delete-'+str(value).lower()
