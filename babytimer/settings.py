46bf3b95d0b776e8d8becf88790b3c3cb80540c6
Robin <135007436+RobinF98@users.noreply.github.com>
2024-03-08 09:31:03 +0000

    Define static_root var in settings

--- babytimer/settings.py
+++ babytimer/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_DIRS = [
     BASE_DIR / "logs/static",
 ]
